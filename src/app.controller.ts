5155b533c68682b963dfbc0723dffd4a66891590
sirsho29 <sirsha.cse@gmail.com>
2026-02-15 11:21:36 +0530

    Add POST endpoint and enhance GET method logging in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('')
   getHello(): string {
+    console.log('HELLO');
+    return this.appService.getHello();
+  }
+
+  @Post('test')
+  getTest(@Req() request: any): string {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    console.log('Request received:', request.body);
     return this.appService.getHello();
   }
 }
